9c87176fab468b9c2182eac24162d33f8459e95a
tommasosansone91 <tommaso.sansone91@virgilio.it>
2024-05-19 01:12:50 +0200

    allowing all hosts

--- simplesocial/settings.py
+++ simplesocial/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = "4590hgjv2f309kfcm4triq9w0j3mfcqr"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
